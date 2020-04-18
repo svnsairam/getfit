56c980c6373fb11e2208d2f5587a683523f4c79c
svnsairam <svnsairam.sakhamuri@gmail.com>
2020-04-18 08:11:41 +0530

    navbar changes

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -27,18 +27,12 @@
           <router-link to="/activities">Activities</router-link>
         </a>
 
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link">More</a>
-
-          <div class="navbar-dropdown">
             <a class="navbar-item">
               <router-link to="/about">About</router-link>
             </a>
             <a class="navbar-item">
               <router-link to="/contact">Contact</router-link>
             </a>
-          </div>
-        </div>
       </div>
 
       <div class="navbar-end">
